aaf5866a1cdb2386c800adebbb4e1a4ddd7de535
Tanha <tanhakabir.ca@gmail.com>
2018-10-07 14:25:31 +0800

    Remove dummy example project

--- Example/Pods/Target Support Files/ColorLog/ColorLog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ColorLog : NSObject
-@end
-@implementation PodsDummy_ColorLog
-@end
--- Example/Pods/Target Support Files/Pods-ColorLog_Tests/Pods-ColorLog_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ColorLog_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ColorLog_Tests
-@end
